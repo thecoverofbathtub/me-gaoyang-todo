0ceb1c9d20b1e006f0653805656d6fd90ae8641e
thecoverofbathtub <gao.yang.ustc@gmail.com>
2016-01-25 22:22:51 -0500

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+// main.js
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+class App extends React.Component {
+    render() {
+        return (
+        	<div>
+        		ALSJFKHDJK
+        	</div>
+        );
+    }
+}
+
+ReactDOM.render(
+	<App />,
+    document.getElementById('app-root')
+);
