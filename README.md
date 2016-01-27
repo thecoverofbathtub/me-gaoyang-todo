cff394817e39c523c8345d36cc601d321fb03209
thecoverofbathtub <gao.yang.ustc@gmail.com>
2016-01-27 00:30:36 -0500

    Really crappy version todo app

--- README.md
+++ README.md
@@ -5,5 +5,8 @@ Yang's Personal Website
 
 ## Installation
 1. `sudo npm install --save react react-dom babelify babel-preset-react`
-2. `sudo npm install -g browserify`
-3. `browserify -t [ babelify --presets [ react ] ] scripts/main.js -o bundle.js`
+2. `sudo npm install --save redux react-redux uuid`
+2. `sudo npm install --save-dev redux-devtools`
+4. `sudo npm install --save-dev babel-preset-es2015 babel-preset-stage-2`
+3. `sudo npm install -g browserify`
+4. `browserify -t [ babelify --presets [ react ] ] scripts/main.js -o bundle.js`
--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,97 @@
 // main.js
-'use strict'
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import uuid from 'uuid'
+
+const todoReducer = (item, action) => {
+	switch (action.type) {
+		case 'ADD':
+			return {
+				id: action.id,
+				text: action.text,
+				completed: false
+			};
+		case 'TOGGLE':
+			if (item.id !== action.id) {
+				return item;
+			}
+			return {
+				...item,
+				completed: !item.completed
+			};
+		default:
+			return item;
+	}
+};
+
+const todosReducer = (items = [], action) => {
+	switch (action.type) {
+		case 'ADD':
+			return [...items, todoReducer(undefined, action)];
+		case 'TOGGLE':
+			return items.map(item => { return todoReducer(item, action); });
+		default:
+			return items;
+	}
+};
+
+const store = createStore(todosReducer);
 
 class App extends React.Component {
+	setDate(data) {
+		this.setSta
+	}
     render() {
+    	console.log("Called render()");
         return (
         	<div>
-        		ALSJFKHDJK
-        	</div>
+	        	<input
+	        		type="text"
+	        		placeholder="Todo"
+	        		ref={ (c) => { this.input = c; }
+	        	} />
+	        	<button onClick={ () => {
+	        		store.dispatch({
+	        			type: 'ADD',
+	        			id: uuid.v4(),
+	        			text: this.input.value
+	        		});
+	        		this.input.value = "";
+	        	}}>
+	        		Add
+	        	</button>
+	        	<ul>
+	        		{this.props.data.map( todo =>
+	        			<li key={todo.id}
+	        				onClick={ () => {
+	        					store.dispatch({
+	        						type: 'TOGGLE',
+	        						id: todo.id
+	        					})
+	        				}}
+	        				style={{
+	        					textDecoration:
+	        						todo.completed ? 'line-through' : 'none'
+	        				}}>
+	        				{todo.text}
+	        			</li>
+	        		)}
+	        	</ul>
+	        </div>
         );
     }
 }
 
-ReactDOM.render(
-	<App />,
-    document.getElementById('app-root')
-);
+const renderAll = () => {
+	ReactDOM.render(
+		<App data={store.getState()} />,
+	    document.getElementById('app-root')
+	);
+};
+
+store.subscribe(() => {
+	console.log(store.getState());
+	renderAll();
+});
+renderAll();
